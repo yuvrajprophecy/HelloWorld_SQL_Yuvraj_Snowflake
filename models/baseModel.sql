0707e2d5c484d80ced6d7a8f181f343d76f619c8
yuvraj@prophecy.io <yuvraj@prophecy.io>
2025-07-14 07:39:42 +0000

    Add baseModel and update customers and orders models in dbt project configuration

---
+++ models/baseModel.sql
@@ -0,0 +1,18 @@
+{{
+  config({    
+    "materialized": "table",
+    "schema":  var('config-1') 
+  })
+}}
+
+WITH raw_payments AS (
+
+  SELECT * 
+  
+  FROM {{ ref('raw_payments')}}
+
+)
+
+SELECT *
+
+FROM raw_payments
